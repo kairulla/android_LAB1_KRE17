b02debfbfc6d63dad4f97c25c4a2c0b77b962e7a
kairulla <kayrulla@bk.ru>
2022-10-11 02:15:03 +0600

    +теперь B исчезает в зависимости от X; resolved #1, resolved #3

--- app/src/main/java/kz/kairulla/lab1_kre17/MainActivity.java
+++ app/src/main/java/kz/kairulla/lab1_kre17/MainActivity.java
@@ -2,18 +2,12 @@ package kz.kairulla.lab1_kre17;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
     EditText editTextA, editTextB, editTextX;
@@ -31,23 +25,44 @@ public class MainActivity extends AppCompatActivity {
         textViewOtvet = (TextView) findViewById(R.id.textViewOtvet);
         buttonSolver = (Button) findViewById(R.id.buttonSolver);
         buttonClear = (Button) findViewById(R.id.buttonClear);
+        editTextB.setVisibility(View.GONE);
 //        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // залочил ориентацию
 
         // Собственный обработчик нажатий на клавиши
         myKeyListener = new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                // она сохраняет содержимое при перевороте
-                if (
-                        editTextA.getText().toString().trim().equals("") ||
-                        editTextB.getText().toString().trim().equals("") ||
-                        editTextX.getText().toString().trim().equals("")
-                ) {
-                    buttonSolver.setEnabled(false); // Выключаем доступность нажатия у кнопки
-                    buttonClear.setEnabled(false);
-                } else {
-                    buttonSolver.setEnabled(true); // Включаем доступность нажатия у кнопки
-                    buttonClear.setEnabled(true);
+                try {
+                    double x = Double.parseDouble(editTextX.getText().toString());
+                    editTextB.setVisibility(x < 8 ? View.GONE : View.VISIBLE);
+                    if (x < 8) {
+                        // она сохраняет содержимое при перевороте
+                        if (
+                                editTextA.getText().toString().trim().equals("") ||
+                                editTextX.getText().toString().trim().equals("")
+                        ) {
+                            buttonSolver.setEnabled(false); // Выключаем доступность нажатия у кнопки
+                            buttonClear.setEnabled(false);
+                        } else {
+                            buttonSolver.setEnabled(true); // Включаем доступность нажатия у кнопки
+                            buttonClear.setEnabled(true);
+                        }
+                    } else {
+                        // она сохраняет содержимое при перевороте
+                        if (
+                                editTextA.getText().toString().trim().equals("") ||
+                                editTextB.getText().toString().trim().equals("") ||
+                                editTextX.getText().toString().trim().equals("")
+                        ) {
+                            buttonSolver.setEnabled(false); // Выключаем доступность нажатия у кнопки
+                            buttonClear.setEnabled(false);
+                        } else {
+                            buttonSolver.setEnabled(true); // Включаем доступность нажатия у кнопки
+                            buttonClear.setEnabled(true);
+                        }
+                    }
+                } catch (Exception ee) {
+                    editTextB.setVisibility(View.GONE);
                 }
                 return false;
             }
@@ -78,26 +93,42 @@ public class MainActivity extends AppCompatActivity {
     public void onClickButtonSolver(View view) {
         double a = 0, b = 0, x = 0, y = 0;
 
-        try {
-            a = Double.parseDouble(editTextA.getText().toString().trim());
-            b = Double.parseDouble(editTextB.getText().toString().trim());
-            x = Double.parseDouble(editTextX.getText().toString().trim());
+        if (x >= 8) {
+            try {
+                a = Double.parseDouble(editTextA.getText().toString().trim());
+                b = Double.parseDouble(editTextB.getText().toString().trim());
+                x = Double.parseDouble(editTextX.getText().toString().trim());
 
-            if (x >= 8) {
-                y = (a * a + 4 * x * x + b) / (2.0 * x);
-            } else {
-                y = a * a - 2 * x * x;
+                if (x >= 8) {
+                    y = (a * a + 4 * x * x + b) / (2.0 * x);
+                } else {
+                    y = a * a - 2 * x * x;
+                }
+                textViewOtvet.setText(String.valueOf(y));
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
+        } else {
+            try {
+                a = Double.parseDouble(editTextA.getText().toString().trim());
+                x = Double.parseDouble(editTextX.getText().toString().trim());
 
-            textViewOtvet.setText(String.valueOf(y));
-        } catch (Exception ex) {
-            ex.printStackTrace();
+                if (x >= 8) {
+                    y = (a * a + 4 * x * x + b) / (2.0 * x);
+                } else {
+                    y = a * a - 2 * x * x;
+                }
+                textViewOtvet.setText(String.valueOf(y));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
     public void onClickButtonClear(View view) {
         editTextA.setText("");
         editTextB.setText("");
+        editTextB.setVisibility(View.GONE);
         editTextX.setText("");
         textViewOtvet.setText("ОТВЕТ:");
         buttonSolver.setEnabled(false); // Выключаем доступность нажатия у кнопки
